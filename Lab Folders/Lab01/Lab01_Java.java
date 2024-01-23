8a53dd813820c189668bf138c5b88b9364b8fff5
Rachel Agyapong <220200923@aston.ac.uk>
2024-01-23 16:18:45 +0000

    Edited Lab01_Java in GitPod

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
   //Edit made by Rachel_220200923
+  //Edit made by Rachel_220200923 in GitPod
 
 
   //Last line print('Good bye')
